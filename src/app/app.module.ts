d81697ef40380842acbc9b5d7cc684060c94ab92
Mateusz Goik <mateuszgoik1@wp.pl>
2022-05-06 19:49:49 +0200

    Add AngularFire package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { environment } from '../environments/environment';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'watering-reminder',
+    appId: '1:1023753988375:web:0ced7a7dcad2e97703af52',
+    storageBucket: 'watering-reminder.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBmbPy7BD8MLRUoL4Zu2ZJavQbRzWldpzQ',
+    authDomain: 'watering-reminder.firebaseapp.com',
+    messagingSenderId: '1023753988375',
+    measurementId: 'G-0YHCHZLE1V',
+  },
   production: false
 };
 
